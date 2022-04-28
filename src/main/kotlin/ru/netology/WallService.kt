1c87f34c2a0fdb9dfff36ea39aaea5bb39d60237
Vladislav Kashpersky <vlakas1978@yandex.ru>
2022-04-28 15:10:26 +0300

    Add files

---
+++ src/main/kotlin/ru/netology/WallService.kt
@@ -0,0 +1,25 @@
+package ru.netology
+
+object WallService {
+    private var posts = emptyArray<Post>()
+    private var postId = 1
+
+
+    fun add(post: Post): Post {
+        val postToAdd = post.copy(id = postId)
+        posts += postToAdd
+        postId++
+        return posts.last()
+
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, postOrigin) in posts.withIndex()) {
+            if (post.id == postOrigin.id) {
+                posts[index] = post.copy(ownedId = posts[index].ownedId, date = posts[index].date)
+                return true
+            }
+        }
+        return false
+    }
+}
